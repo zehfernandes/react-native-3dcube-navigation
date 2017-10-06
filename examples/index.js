c5839823e85ae92b129b89fbc4ea56e2c3995e1b
zehfernandes <ozehfernandes@gmail.com>
2017-10-06 17:06:36 -0300

    Clean examples

--- examples/index.js
+++ examples/index.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 import CubeHorizontal from './components/CubeHorizontal.js'
 import CubeVertical from './components/CubeVertical.js'
 
-AppRegistry.registerComponent('examples', () => CubeVertical);
+AppRegistry.registerComponent('examples', () => CubeHorizontal);
